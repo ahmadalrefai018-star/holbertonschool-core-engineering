d83a922c8910fb20bb86d4029fd999c505bf6470
ahmad.alrefai018-star <ahmad,alrefai018@gmail.com>
2026-09-19 14:21:55 +0300

    Add uppercase function

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    for character in str:
+        if ord("a") <= ord(character) <= ord("z"):
+            character = chr(ord(character) - 32)
+        print("{}".format(character), end="")
+    print()
