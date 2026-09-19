14ea4993d47359142cac7a049abc9737e93ee550
ahmad.alrefai018-star <ahmad,alrefai018@gmail.com>
2026-09-19 13:44:34 +0300

    Add lowercase alphabet script

---
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+alphabet = ""
+
+for number in range(97, 123):
+    letter = chr(number)
+    if letter != "e" and letter != "q":
+        alphabet += letter
+
+print(alphabet)
