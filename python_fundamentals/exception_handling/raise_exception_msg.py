dcd1665b70185de893f65eb0bc530b661d0a7f5b
Ahmed Alrefai <ahmad.alrefai018@gmail.com>
2026-09-25 17:35:56 +0300

    Add custom NameError function

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Raise a NameError with a custom message."""
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError containing the supplied message."""
+    raise NameError(message)
