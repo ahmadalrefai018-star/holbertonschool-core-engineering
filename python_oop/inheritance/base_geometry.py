cbe911cc84f9c13978315c9525be23c417125e5e
Ahmed Alrefai <ahmad.alrefai018@gmail.com>
2026-09-26 14:56:40 +0300

    Add BaseGeometry class

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Define the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represent the base class for geometric shapes."""
+
+    def area(self):
+        """Raise an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
